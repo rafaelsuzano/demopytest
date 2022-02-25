67ff4d0c19573fd5e0f133b3beadb2fac59c939d
Rafael Suzano <rafaelsuzano@hotmail.com>
2022-02-25 15:19:43 -0300

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,63 @@
+import urllib3
+import requests
+import json
+import pytest
+
+#python -m pytest -v demo.py -W error::UserWarning --html=report\report.html
+#python -m pytest -v demo.py --disable-warnings  --html=report\report.html
+urllib3.disable_warnings()
+
+
+url ="https://jsonplaceholder.typicode.com/todos/1"
+
+
+class Test_API():
+
+    @pytest.mark.order(1) 
+    def test_Get_Status200(self):
+
+        #C1 = Valida status =  200
+        r = requests.get(url, verify=False)
+        print("Response:"+str(r.content))
+        print("Code HTTP:"+str(r.status_code))
+        assert r.status_code == 200, f"Erro no http status code"
+        
+    @pytest.mark.order(2)    
+    def test_Put_InserirDados(self):
+        url= "https://jsonplaceholder.typicode.com/posts/2"
+        body = {
+            "id":1,
+            "title": 'teste demo',
+            "body": 'bar',
+            "userId":1
+        }
+        body = json.dumps(body)
+
+
+        headers= {'Content-type': 'application/json; charset=UTF-8'}
+
+        r = requests.put(url, data=body,headers=headers)
+        print("Response:"+str(r.content))
+        print("Code HTTP:"+str(r.status_code))
+        assert r.status_code == 200, f"Erro no http status code"
+    
+    @pytest.mark.order(3) 
+    def test_Delete_InserirDados(self):
+        url= "https://jsonplaceholder.typicode.com/posts/2"
+        r1 = requests.delete(url)
+        print("Response:"+str(r1.content))
+        print("Code HTTP:"+str(r1.status_code))
+        assert r1.status_code == 200, f"Erro no http status code"
+
+    @pytest.mark.order(4) 
+    def test_Verifica_Dados_Json(self):
+        url ="https://jsonplaceholder.typicode.com/todos/1"
+        json_data = requests.get(url, verify=False).json()
+        print(json_data)
+        print(json_data['completed'])
+        completed=(json_data['completed'])
+        assert completed == True, f"Retorno do completed era esperado True"
+    
+    @pytest.mark.skip(reason="Próximos testes")    
+    def test_ProximosTeste(self):
+        pass
