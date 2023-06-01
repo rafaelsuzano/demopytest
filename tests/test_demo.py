98af345f3477c810b1f5c4b727bdd6e947783b90
Rafael Suzano <rafaelsuzano@hotmail.com>
2023-06-01 13:15:21 +0100

    Update test_demo.py

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -69,5 +69,5 @@ class Test_API():
         print(json_data['completed'])
         completed=(json_data['completed'])
         assert True == False, f"Retorno do completed era esperado True"
-os._exit(1)       
+os._exit(0)       
         
